4e5fca251af51b10315b8a2fd91b641bf008498e
Prajjawalk <prajjawalkhandelwal@gmail.com>
2022-09-14 19:42:06 +0530

    changes in web3-zond-ens and web3-core-requestmanager

--- packages/@theqrl/web3-zond-ens/src/ResolverMethodHandler.js
+++ packages/@theqrl/web3-zond-ens/src/ResolverMethodHandler.js
@@ -1,28 +1,22 @@
 /*
     This file is part of web3.js.
-    web3.js is free software: you can redistribute it and/or modify
+    @theqrl/web3.js is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    web3.js is distributed in the hope that it will be useful,
+    @theqrl/web3.js is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
     You should have received a copy of the GNU Lesser General Public License
-    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+    along with @theqrl/web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-/**
- * @file ResolverMethodHandler.js
- *
- * @author Samuel Furter <samuel@ethereum.org>
- * @date 2018
- */
 
 "use strict";
 
-var PromiEvent = require('web3-core-promievent');
+var PromiEvent = require('@theqrl/web3-core-promievent');
 var namehash = require('eth-ens-namehash');
-var errors = require('web3-core-helpers').errors;
+var errors = require('@theqrl/web3-core-helpers').errors;
 var interfaceIds = require('./config').interfaceIds;
 
 /**
